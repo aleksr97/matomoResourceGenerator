6134f52f31b64bd2ce9d1d2382635f26664f39a1
Aleksandar Rajic <aleks.rajic97@gmail.com>
2023-10-25 14:36:21 +0200

    backendRepo initialized

---
+++ matomoResourceGeneratorBackend/src/main/kotlin/com/aleksR97/matomoResourceGeneratorBackend/MatomoResourceGeneratorBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.aleksR97.matomoResourceGeneratorBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatomoResourceGeneratorBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatomoResourceGeneratorBackendApplication>(*args)
+}
---
+++ matomoResourceGeneratorBackend/src/test/kotlin/com/aleksR97/matomoResourceGeneratorBackend/MatomoResourceGeneratorBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aleksR97.matomoResourceGeneratorBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatomoResourceGeneratorBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
